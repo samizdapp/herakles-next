99609331c9a3f9cf1318288c4ef3fa0f87abd7e7
Ryan Bennett <nomad.ry@gmail.com>
2022-06-22 03:50:14 -0600

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,11 @@ import HomeLayout from "../layouts/home";
 import { GetServerSideProps } from "next";
 import { isSupportedPlatform } from "../lib/support";
 import platform from "platform";
-import Storage from "herakles-lib/dist/storage.web";
-import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [res, setRes] = useState("waiting");
-  useEffect(() => {
-    const s = new Storage("next");
-    console.log("aldkfjald");
-    s.setItem("test", { payload: "test" })
-      .then((_r: any) => s.getItem("test"))
-      .then((val: any) => setRes(val));
-  }, []);
   return (
     <HomeLayout>
-      <h1>Index: {JSON.stringify(res)}</h1>
+      <h1>Home</h1>
     </HomeLayout>
   );
 }
