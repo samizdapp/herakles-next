246d32049be3296c8659e408da7389a76b775c10
Ryan Bennett <nomad.ry@gmail.com>
2022-07-14 00:53:53 -0600

    fix config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 // /** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
-const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = withPWA({
   pwa: {
